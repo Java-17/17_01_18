bb7e459574089fcfa389eb9b5f618167e8e55b9f
Gregory Sheygam <sheygam@gmail.com>
2018-01-17 22:58:32 +0200

    first commit

---
+++ app/src/main/java/com/sheygam/java_17_17_01_18/MainActivity.java
@@ -0,0 +1,20 @@
+package com.sheygam.java_17_17_01_18;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MyFragment fragment = new MyFragment();
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.fragment_container,fragment);
+        transaction.commit();
+    }
+}
